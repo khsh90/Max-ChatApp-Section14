1f815fdfb16fbbc82c3215092639c190010422c8
Khaled Hadidi <khsh400@gmail.com>
2021-09-28 14:57:40 +0300

    add stream bulider in order to fetch the data from firestore

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,23 +5,39 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) => Container(
-          padding: EdgeInsets.all(10),
-          child: Text("chat app"),
-        ),
-      ),
+      body: StreamBuilder(
+          //used to fetch the data from firestore as Stream bulider is flutter class
+          stream: Firestore.instance
+              .collection('chat/IQD8Vas22WURM65aA685/messages')
+              .snapshots(),
+          builder: (context, snapshot) {
+            final documents = snapshot.data.documents;
+            //this if used as the snap shot at begining is empty
+            if (snapshot.connectionState == ConnectionState.waiting)
+              Center(
+                child: CircularProgressIndicator(),
+              );
+
+            return ListView.builder(
+              itemCount: documents.length,
+              itemBuilder: (context, index) => Container(
+                padding: EdgeInsets.all(10),
+                child: Text(documents[index]['text']),
+              ),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Firestore.instance
-              .collection('chat/IQD8Vas22WURM65aA685/messages')
-              .snapshots()
-              .listen((event) {
-            event.documents.forEach((element) {
-              print(element['text']);
-            });
-          });
+          // Firestore.instance
+          //     .collection('chat/IQD8Vas22WURM65aA685/messages')
+          //     .snapshots()
+          //     .listen((event) {
+          //   event.documents.forEach((element) {
+          //     print(element['text']);
+          //   }
+          //   );
+
+          // });
         },
         child: Icon(Icons.add),
       ),
