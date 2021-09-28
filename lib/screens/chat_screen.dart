934a40327dfb47191109ace171256e3aa5c3f5eb
Khaled Hadidi <khsh400@gmail.com>
2021-09-28 13:51:38 +0300

    add cloud firestore ,fetch the data from fire store firebase db in chat_screen.dart

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView.builder(
-      padding: EdgeInsets.all(10),
-      itemCount: 10,
-      itemBuilder: (context, index) => Container(
-        child: Text("chat app"),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) => Container(
+          padding: EdgeInsets.all(10),
+          child: Text("chat app"),
+        ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Firestore.instance
+              .collection('chat/IQD8Vas22WURM65aA685/messages')
+              .snapshots()
+              .listen((event) {
+            event.documents.forEach((element) {
+              print(element['text']);
+            });
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
